298b003e6e09d8987af3871093d5f959c91a86da
DantheCE <dan.doyin@gmail.com>
2026-05-04 14:33:04 +0100

    feat: implement _strstr function to locate a substring within a string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,18 +10,20 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0, j = 0;
+	int i, j;
+
+	if (*needle == 0)
+		return (haystack);
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[j] == haystack[i])
-			{
-				return (needle + j);
-			}
-			break;
+			if (haystack[i + j] != needle[j])
+				break;
 		}
+		if (needle[j] == '\0')
+			return (haystack + i);
 	}
 	return (NULL);
 }

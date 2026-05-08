b5249d309dd4157eb83e9683f5cf18da10ab574f
DantheCE <dan.doyin@gmail.com>
2026-05-08 13:40:26 +0100

    feat: implement recursive string length calculation and update header prototypes

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,9 +2,11 @@
 #define FUNC_H
 
 #include <stddef.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 
 #endif /* FUNC_H */

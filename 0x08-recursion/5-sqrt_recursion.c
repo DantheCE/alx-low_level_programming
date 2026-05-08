e79d1e7264739575c3d4e58ab93ae8ff56497430
DantheCE <dan.doyin@gmail.com>
2026-05-08 19:23:13 +0100

    feat: implement recursive square root calculation using a helper function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -29,23 +29,3 @@ int _sqrt_recursion(int n)
 {
 	return (guess_checker(n, 1));
 }
-#include "main.h"
-
-/**
- * factorial - function to calculate factorial recursively
- * @n: number to calculate factorial for
- *
- * Return: factorial of a number
- */
-int factorial(int n)
-{
-	if (n < 0)
-	{
-		return (-1);
-	}
-	if (n == 0)
-	{
-		return (1);
-	}
-	return (n * factorial(n - 1));
-}

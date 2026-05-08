c00a6c82f510f7d0435405035eec0f6d15efc8ba
DantheCE <dan.doyin@gmail.com>
2026-05-09 00:06:37 +0100

    feat: implement recursive palindrome check function and update header file declarations

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * is_palindrome_check - logic engine for palindrome checker
+ * @first: pointer to the first char of string
+ * @last: pointer to the last char of string
+ *
+ * Return: 1 if word is palindrome, 0 if not
+ */
+int is_palindrome_check(char *first, char *last)
+{
+	if (first >= last)
+	{
+		return (1);
+	}
+	if (*first != *last)
+	{
+		return (0);
+	}
+	return (is_palindrome_check(first + 1, last - 1));
+}
+/**
+ * is_palindrome - checks if a word is a palindrome
+ * @s: pointer to the first char of string
+ *
+ * Return: 1 if word is palindrome, 0 if not
+ */
+int is_palindrome(char *s)
+{
+	int length = strlen(s);
+	if (length == 0 || length == 1)
+	{
+		return (1);
+	}
+	return (is_palindrome_check(s, s + length - 1));
+}

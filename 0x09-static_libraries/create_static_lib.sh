f7c4754ab0d0dbc235116e9b1abe467f6c315cf5
DantheCE <dan.doyin@gmail.com>
2026-05-14 11:55:05 +0100

    feat: create bash script to compile C files into a static library liball.a

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Script to populate the 0x09-static_libraries with the requested functions, compile them to relocatable objects, and package them into libmy.a
-
-echo "Compiling .c files to .o using gcc -c..."
-gcc -c -std=gnu89 *.c
-
-echo "Creating the static library libmy.a..."
+gcc -c -Wall -Werror -Wextra -pedantic -std=gnu89 *.c
 ar rcs liball.a *.o
-
-echo "Done! The liball.a has been generated with all required relocatable object files."

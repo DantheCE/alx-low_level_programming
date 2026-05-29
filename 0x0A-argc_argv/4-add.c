005a4a6ab85cb8e26121bcdf78825dd0a0080039
DantheCE <dan.doyin@gmail.com>
2026-05-29 21:37:21 +0100

    feat: add programs to multiply and add command-line arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,16 +11,32 @@
  */
 int main(int argc, char *argv[])
 {
-	int result;
+	int result = 1;
+	int neg = 0;
+	int i = 1;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	result = atoi(argv[1]) * atoi(argv[2]);
+	while (i < argc)
+	{
+		if (*argv[i] == '-')
+		{
+			neg++;
+			result *= strtol(argv[i] + 1, argv, 10);
+			i++;
+			continue;
+		}
+		result *= strtol(argv[i], argv, 10);
+		i++;
+	}
+	if (neg % 2)
+	{
+		printf("%c%d\n", '-', result);
+		return (0);
+	}
 	printf("%d\n", result);
-
 	return (0);
 }

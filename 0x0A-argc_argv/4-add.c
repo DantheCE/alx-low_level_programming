b2941010f895828b6f17c713c579f7f68eb6f185
DantheCE <dan.doyin@gmail.com>
2026-06-02 11:34:26 +0100

    refactor: Changed implementation of 4-add.c to pointer only

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,30 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * validateNumber - checks if a string contains only digits
+ * @chNum: string to validate
+ * @length: length of the string
+ *
+ * Return: 0 on success, 1 on error
+ */
+int validateNumber(char *chNum, int length)
+{
+	int i = 0;
+
+	for (i = 0; i < length; i++)
+	{
+		if (isdigit(chNum[i]))
+		{
+			continue;
+		}
+		return (1);
+	}
+	return (0);
+}
 
 /**
  * main - adds positive numbers
@@ -11,27 +35,22 @@
  */
 int main(int argc, char *argv[])
 {
-	int result = 0, i = 0, j = 0;
+	int result = 0, i = 0;
 
 	if (argc == 1)
 	{
 		printf("%d\n", 0);
-		return (1);
+		return (0);
 	}
 	argv++;
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; **argv; j++)
+		if (validateNumber(*argv, strlen(*argv)))
 		{
-			if (atoi(*argv) || **argv == '0')
-			{
-				result += atoi(*argv);
-				(*argv)++;
-				continue;
-			}
-			printf("Error\n");
-			return (1);
+		printf("Error\n");
+		return (1);
 		}
+		result += atoi(*argv);
 		argv++;
 	}
 	printf("%d\n", result);
